26cb0578407fa53f853c91e9e731e128a64bf275
tshiamo <tslephal>
2022-03-12 02:07:19 +0200

    Spring initializer for server

---
+++ GadgetHomeServer/src/main/java/za/co/wethinkcode/gadgethomeserver/GadgetHomeApplication.java
@@ -0,0 +1,13 @@
+package za.co.wethinkcode.gadgethomeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadgetHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GadgetHomeApplication.class, args);
+    }
+
+}
---
+++ GadgetHomeServer/src/test/java/za/co/wethinkcode/gadgethomeserver/GadgetHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.wethinkcode.gadgethomeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadgetHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
